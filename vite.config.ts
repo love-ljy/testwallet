0738e58625f14dbefa30dfd5ed3ffe8621ed4305
love-ljy <73459953+love-ljy@users.noreply.github.com>
2024-11-06 15:04:24 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     outDir: 'docs'
   },
   // @ts-ignore
-  base: process.env.GH_PAGES ? '/demo-dapp-with-wallet/' : './',
+  base: 'textwallet',
   server: {
     fs: {
       allow: ['../sdk', './'],
